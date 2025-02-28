628424edfcf36dbb36565a3d756060345fee751e
Victor <victorhenriquebl11@gmail.com>
2025-02-28 15:11:41 -0300

    feat: Creating StockMoviment entity

---
+++ src/main/java/com/victor/inventoryManager/entity/StockMovement.java
@@ -0,0 +1,72 @@
+package com.victor.inventoryManager.entity;
+
+import com.victor.inventoryManager.entity.enums.MovimentType;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_stock_moviment")
+public class StockMovement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private MovimentType type;
+
+    @Column(name = "created_at", updatable = false, columnDefinition = "TIMESTAMP DEAFULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    public StockMovement() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public MovimentType getType() {
+        return type;
+    }
+
+    public void setType(MovimentType type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
