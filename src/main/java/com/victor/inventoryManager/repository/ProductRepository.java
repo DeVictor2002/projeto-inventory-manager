87b01fdb62edfca3f5f1953d7515c2ddb2d265f7
Victor <victorhenriquebl11@gmail.com>
2025-02-28 16:09:30 -0300

    refactor: Change name

--- src/main/java/com/victor/inventoryManager/repository/ProductRepository.java
+++ src/main/java/com/victor/inventoryManager/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.victor.inventoryManager.repository;
 import com.victor.inventoryManager.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRespository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
