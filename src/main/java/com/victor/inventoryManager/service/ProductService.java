a2727e037a58491055fbc52ea10a971c913ae4a3
Victor <victorhenriquebl11@gmail.com>
2025-02-28 16:13:53 -0300

    refactor: Using streams in method return

--- src/main/java/com/victor/inventoryManager/service/ProductService.java
+++ src/main/java/com/victor/inventoryManager/service/ProductService.java
@@ -1,27 +1,53 @@
 package com.victor.inventoryManager.service;
 
+import com.victor.inventoryManager.dto.CreateProductDto;
+import com.victor.inventoryManager.dto.ProductDto;
 import com.victor.inventoryManager.entity.Product;
-import com.victor.inventoryManager.repository.ProductRespository;
+import com.victor.inventoryManager.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ProductService {
 
-    private final ProductRespository productRespository;
+    private final ProductRepository productRepository;
 
-    public ProductService(ProductRespository productRespository) {
-        this.productRespository = productRespository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
-    public Page<Product> findAll(Integer page, Integer pageSize) {
+    public Page<ProductDto> findAll(Integer page, Integer pageSize) {
 
         var pageRequest = PageRequest.of(page, pageSize);
 
-        return productRespository.findAll(pageRequest);
+        Page<Product> products = productRepository.findAll(pageRequest);
+
+        return products.map(product -> new ProductDto(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getQuantity(),
+                product.getPrice(),
+                product.getCategory() != null ? product.getCategory().getName() : null
+        ));
     }
 
+    public ProductDto createProduct(CreateProductDto createProductDto) {
+
+        Product product = new Product();
+        product.setName(createProductDto.name());
+        product.setDescription(createProductDto.description());
+        product.setQuantity(createProductDto.quantity());
+        product.setPrice(createProductDto.price());
+
+        Product productSaved = productRepository.save(product);
+
+        return new ProductDto(productSaved.getId(),
+                productSaved.getName(),
+                productSaved.getDescription(),
+                productSaved.getQuantity(),
+                productSaved.getPrice(),
+                productSaved.getCategory().getName());
+    }
 }
