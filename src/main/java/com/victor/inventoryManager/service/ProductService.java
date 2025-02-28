1b1c5e65a2dc192b24ab0fd1dd5d4a0e828e6672
Victor <victorhenriquebl11@gmail.com>
2025-02-28 17:18:50 -0300

    feat: adding Category search for validation

--- src/main/java/com/victor/inventoryManager/service/ProductService.java
+++ src/main/java/com/victor/inventoryManager/service/ProductService.java
@@ -2,7 +2,10 @@ package com.victor.inventoryManager.service;
 
 import com.victor.inventoryManager.dto.CreateProductDto;
 import com.victor.inventoryManager.dto.ProductDto;
+import com.victor.inventoryManager.entity.Category;
 import com.victor.inventoryManager.entity.Product;
+import com.victor.inventoryManager.exception.CategoryNotFoundException;
+import com.victor.inventoryManager.repository.CategoryRepository;
 import com.victor.inventoryManager.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,9 +15,11 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Page<ProductDto> findAll(Integer page, Integer pageSize) {
@@ -35,11 +40,15 @@ public class ProductService {
 
     public ProductDto createProduct(CreateProductDto createProductDto) {
 
+        Category category = categoryRepository.findById(createProductDto.categoryId())
+                .orElseThrow(() -> new CategoryNotFoundException("Categoria não encontrada"));
+
         Product product = new Product();
         product.setName(createProductDto.name());
         product.setDescription(createProductDto.description());
         product.setQuantity(createProductDto.quantity());
         product.setPrice(createProductDto.price());
+        product.setCategory(category);
 
         Product productSaved = productRepository.save(product);
 
